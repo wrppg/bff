1fea94b3f0663c93ce46bcbc9c1eaadc07f8b5dd
wrppg <pg.worapon@gmail.com>
2025-09-01 17:02:48 +0700

    Update t.sh

--- t.sh
+++ t.sh
@@ -2,4 +2,6 @@
 pwd
 curl -o yq -L https://github.com/mikefarah/yq/releases/download/v4.47.1/yq_linux_amd64
 chmod +x yq bff/t.sh
-./yq 2>&1
+mkdir "$HOME"/bin
+echo "$HOME"/bin >> $GITHUB_PATH
+./yq
