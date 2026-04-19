4cce8199a46d4f3b9c815dbb564cb8baa0d38ad3
wrppg <pg.worapon@gmail.com>
2026-04-19 21:45:28 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -95,9 +95,9 @@ function RootActivityLauncher_Extra_Setup {
 	# API_LEVEL=$((curl -sLf https://raw.githubusercontent.com/zacharee/RootActivityLauncher/refs/heads/master/app/build.gradle.kts \
 	# 	|| curl -sLf https://raw.githubusercontent.com/zacharee/RootActivityLauncher/refs/heads/master/app/build.gradle) \
 	# 	| awk '/^\s*compileSdk( |=)/ {print $NF; exit}')
-	
- 	API_LEVEL=$(awk '/^\s*compileSdk( |=)/ {print $NF; exit}' \
-  	$(find app -maxdepth 1 \( -name 'build.gradle' -or -name 'build.gradle.kts' \) -print -quit))
+
+	ANDROID_MANIFEST=$(find app -maxdepth 1 \( -name 'build.gradle' -or -name 'build.gradle.kts' \) -print -quit)
+ 	API_LEVEL=$(awk '/^\s*compileSdk\s*[ =]/ {print $NF; exit}' $ANDROID_MANIFEST)
 	
  	[[ "${API_LEVEL}" =~ ^[0-9]+$ ]] || { echo '❌ Invalid API_LEVEL'; exit 99; }
 
